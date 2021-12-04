fca190cbb300e6987475daca8cf1e968fe5b0759
Jeremy Castagno <jeremybyu@gmail.com>
2021-12-04 08:48:38 -0500

    Loading Images

--- examples/canny_serial.cpp
+++ examples/canny_serial.cpp
@@ -5,6 +5,14 @@
 #include <map>
 #include <string>
 
+#include "PCanny/PCanny.hpp"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb/stb_image.h"
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb/stb_image_write.h"
+
 
 void usage(char* s) {
     fprintf(stderr, "\n");
@@ -28,6 +36,7 @@ int main(int argc, char** argv) {
     int c;
     
     std::string img_path;
+    std::string output_image_path;
     int low_threshold = 30;
     int high_threshold = 100;
     while (((c = getopt(argc, argv, "s:l:h:?"))) != -1) {
@@ -53,6 +62,28 @@ int main(int argc, char** argv) {
         return -1;
     }
 
+    output_image_path = img_path.substr(0, img_path.length() - 4) + "_edges.png";
+    int x,y,n;
+    unsigned char *data = stbi_load(img_path.c_str(), &x, &y, &n, 0);
+    std::cout << "Size: (" << x << "x" << y << "x" << n << ")" << std::endl;
+
+    // int w = img_gray.cols;
+    // int h = img_ori.rows;
+
+    // PCanny::Canny::NoiseFilter filter = keymolen::Canny::NoiseFilter::Gaus3x3;
+
+    // while (1) {
+    //     cv::Mat img_edge(h, w, CV_8UC1, cv::Scalar::all(0));
+
+    //     PCanny::Canny canny(w, h);
+    //     canny.edges(img_edge.data, img_gray.data, filter, low_threshold,
+    //                 high_threshold);
+
+    unsigned char *image_edges = static_cast<unsigned char *>(calloc(x * y, sizeof(unsigned char)));
+
+    stbi_write_png(output_image_path.c_str(), x, y, 1, image_edges, 1);
+    
+    stbi_image_free(data);
 
     return 0;
 }
